6d906aa1e706de1c4479d4ed4d5d9a623065993b
Louis Luo <louis.luo@gosugamers.net>
2024-12-24 15:11:00 +0800

    Add page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <div style={{
+        display:'flex',
+        gap:"8px"
+      }}>
+        <Link href="/images" prefetch={false}>Icon by webp</Link>
+        <Link href="/svg" prefetch={false}>Icon by svg</Link>
+      </div>
+
     </div>
   );
 }
---
+++ src/app/svg/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+  console.log("image")
+  return (
+    <div>
+
+    </div>
+  );
+}
