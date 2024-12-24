aff2b3f9a302089de4d5753a1a98816db499f403
Louis Luo <louis.luo@gosugamers.net>
2024-12-24 15:17:11 +0800

    Update page layout

--- src/app/images/page.tsx
+++ src/app/images/page.tsx
@@ -3,7 +3,9 @@ export default function Page() {
   console.log("image")
   return (
     <div>
+      <div style={{ minWidth: "300px", minHeight:"300px", margin: "0 auto" }}>
 
+      </div>
     </div>
   );
 }
--- src/app/svg/page.tsx
+++ src/app/svg/page.tsx
@@ -1,9 +1,11 @@
 
 export default function Page() {
-  console.log("image")
+  console.log("svg")
   return (
     <div>
+      <div style={{ minWidth: "300px", minHeight:"300px", margin: "0 auto" }}>
 
+      </div>
     </div>
   );
 }
