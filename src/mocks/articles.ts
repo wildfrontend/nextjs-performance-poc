532ce4f5b276b7c3c6bb4349d938c4117461060e
Louis Luo <louis.luo@gosugamers.net>
2024-12-26 15:35:56 +0800

    Add articles

---
+++ src/mocks/articles.ts
@@ -0,0 +1,213 @@
+const articles = [
+  {
+    id: 69601,
+    frontendId: 1,
+    type: 1,
+    typeName: 'News',
+    urlSafeType: 'news',
+    title: 'Test122',
+    urlSafeTitle: 'louis-test',
+    sectionIds: [9],
+    displaySectionId: 9,
+    displaySectionUrlSafeName: 'dota2',
+    publishedAt: 1734418320000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/9b/f9/a5/46e5264aed818d849590f215ac317da6836db53f8c7e34ffe4675bdcf2.webp',
+    headlineImageText: 'Test1',
+    squareHeadlineImageUrl: null,
+    teaser: 'Test1123',
+    url: '/dota2/news/69601-louis-test',
+  },
+  {
+    id: 69598,
+    frontendId: 1,
+    type: 2,
+    typeName: 'Features',
+    urlSafeType: 'features',
+    title:
+      'Chips begone! What is the best food to improve your gaming performance?',
+    urlSafeTitle: 'test-two',
+    sectionIds: [9, 10, 13, 20, 21, 22, 23, 25],
+    displaySectionId: 11,
+    displaySectionUrlSafeName: 'general',
+    publishedAt: 1733129520000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/2c/ba/e4/25ee61844de6e2b4816a8499d27ce9c9d7f2c3ebbfa99f1747e5b7e2df.webp',
+    headlineImageText: 'Food for gamer',
+    squareHeadlineImageUrl: null,
+    teaser:
+      'Forget about your soda, chips, and sugary food. These are the best foods you can eat to improve your gaming performance!',
+    url: '/features/69598-test-two',
+  },
+  {
+    id: 69597,
+    frontendId: 1,
+    type: 1,
+    typeName: 'News',
+    urlSafeType: 'news',
+    title:
+      'PUBG Mobile introduces Ultimate Royale, an esports-like mode in ranked mode',
+    urlSafeTitle:
+      'pubg-mobile-introduces-ultimate-royale-an-esports-like-mode-in-ranked-mode',
+    sectionIds: [20, 25],
+    displaySectionId: 11,
+    displaySectionUrlSafeName: 'general',
+    publishedAt: 1733129340000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/c4/19/ec/dd983c02813228feab3c0a20ce8411c622907570d93fa002d929acbeb3.webp',
+    headlineImageText: 'PUBG Mobile',
+    squareHeadlineImageUrl: null,
+    teaser:
+      "Level Infinite's Battle Royale game, PUBG Mobile, introduced a new mode with an esports-esque feel, Ultimate Royale.",
+    url: '/news/69597-pubg-mobile-introduces-ultimate-royale-an-esports-like-mode-in-ranked-mode',
+  },
+  {
+    id: 69596,
+    frontendId: 1,
+    type: 2,
+    typeName: 'Features',
+    urlSafeType: 'features',
+    title: "Can Gen.G Esports be South Korea's next big thing?",
+    urlSafeTitle: 'can-gen-g-esports-be-south-korea-s-next-big-thing',
+    sectionIds: [10, 25],
+    displaySectionId: 11,
+    displaySectionUrlSafeName: 'general',
+    publishedAt: 1733110020000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/d0/9a/0e/2a461d8526b5f4530d0d1c4049713528a46a37ff0917fb8878f468939c.webp',
+    headlineImageText: "Can Gen.G Esports be South Korea's next big thing?",
+    squareHeadlineImageUrl: null,
+    teaser:
+      'Gen.G Esports as an org has been one of the best in the world, especially in games like League of Legends and VALORANT.',
+    url: '/features/69596-can-gen-g-esports-be-south-korea-s-next-big-thing',
+  },
+  {
+    id: 69595,
+    frontendId: 1,
+    type: 1,
+    typeName: 'News',
+    urlSafeType: 'news',
+    title: 'Worlds 2024: JunJia says mindset is PSG Talon’s biggest asset',
+    urlSafeTitle:
+      'worlds-2024-junjia-says-mindset-is-psg-talon-s-biggest-asset',
+    sectionIds: [10],
+    displaySectionId: 10,
+    displaySectionUrlSafeName: 'lol',
+    publishedAt: 1733109660000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/75/3b/a7/a3ec383aa42ea9ee26778d544ec3c5836eb1532af145a0be8fe03a837a.webp',
+    headlineImageText: 'JunJia',
+    squareHeadlineImageUrl: null,
+    teaser:
+      'After two years, PSG Talon is finally back in Worlds. JunJia shares his thoughts on how they made it this far.',
+    url: '/lol/news/69595-worlds-2024-junjia-says-mindset-is-psg-talon-s-biggest-asset',
+  },
+  {
+    id: 69594,
+    frontendId: 1,
+    type: 1,
+    typeName: 'News',
+    urlSafeType: 'news',
+    title: 'AVULUS are crowned RES Regional Champions',
+    urlSafeTitle: 'avulus-are-crowned-res-regional-champions',
+    sectionIds: [9],
+    displaySectionId: 9,
+    displaySectionUrlSafeName: 'dota2',
+    publishedAt: 1731035880000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/cc/7f/f4/869baf27dede903cf1124c83fdf2b5c0b265b55a4b48fac9a6599f9266.webp',
+    headlineImageText: 'AVULUS, RES Regional Champions',
+    squareHeadlineImageUrl: null,
+    teaser:
+      'After a yearlong of RES Regional Series, Relog Media concluded its circuit with a LAN tournament held this week in Belgrade, Serbia.',
+    url: '/dota2/news/69594-avulus-are-crowned-res-regional-champions',
+  },
+  {
+    id: 69593,
+    frontendId: 1,
+    type: 1,
+    typeName: 'News',
+    urlSafeType: 'news',
+    title:
+      '‘I am the best in the world,’ says ATF as Falcons claim the BetBoom Dacha Belgrade 2024 trophy',
+    urlSafeTitle:
+      '-i-am-the-best-in-the-world-says-atf-as-falcons-claim-the-betboom-dacha-belgrade-2024-trophy',
+    sectionIds: [9],
+    displaySectionId: 9,
+    displaySectionUrlSafeName: 'dota2',
+    publishedAt: 1731035640000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/27/d6/da/820ed96f3856e483300bd420d916e47d38ec8c1843adcaba772e57d417.webp',
+    headlineImageText: 'ATF, Team Falcons',
+    squareHeadlineImageUrl: null,
+    teaser:
+      'BetBoom Dacha Belgrade 2024 came to an end last night with a thriller best-of-five series.',
+    url: '/dota2/news/69593--i-am-the-best-in-the-world-says-atf-as-falcons-claim-the-betboom-dacha-belgrade-2024-trophy',
+  },
+  {
+    id: 69592,
+    frontendId: 1,
+    type: 2,
+    typeName: 'Features',
+    urlSafeType: 'features',
+    title: 'The plight of Ringmaster in Dota 2 pubs',
+    urlSafeTitle: 'the-plight-of-ringmaster-in-dota-2-pubs',
+    sectionIds: [9],
+    displaySectionId: 9,
+    displaySectionUrlSafeName: 'dota2',
+    publishedAt: 1731035400000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/e0/9c/ff/b256c4ad4eec334348ebf3b6a0737b7ef1099458a8c40b5ab0540196b0.webp',
+    headlineImageText: 'Dota 2, Ringmaster, Ringmaster in pubs',
+    squareHeadlineImageUrl: null,
+    teaser:
+      'After his much awaited release as the newest Dota 2 hero towards the end of August 2024, Ringmaster has been on a downward trajectory in pubs.',
+    url: '/dota2/features/69592-the-plight-of-ringmaster-in-dota-2-pubs',
+  },
+  {
+    id: 69591,
+    frontendId: 1,
+    type: 2,
+    typeName: 'Features',
+    urlSafeType: 'features',
+    title:
+      'Dota 2 statistics: Do professional Dota 2 games fall into a normal distribution?',
+    urlSafeTitle:
+      'dota-2-statistics-do-professional-dota-2-games-fall-into-a-normal-distribution',
+    sectionIds: [9],
+    displaySectionId: 9,
+    displaySectionUrlSafeName: 'dota2',
+    publishedAt: 1731034740000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/75/54/c5/9c2513fc8fce824d717980540a53bb6101f808c2396e75f82d386f6aad.webp',
+    headlineImageText: 'Dota 2, professional Dota 2, Dota 2 statistics',
+    squareHeadlineImageUrl: null,
+    teaser:
+      'With years of data from professional Dota 2 in our hands, we take a look at how closely games adhere to a normal distribution, also known as the bell curve.',
+    url: '/dota2/features/69591-dota-2-statistics-do-professional-dota-2-games-fall-into-a-normal-distribution',
+  },
+  {
+    id: 69590,
+    frontendId: 1,
+    type: 2,
+    typeName: 'Features',
+    urlSafeType: 'features',
+    title:
+      'No rest for the wicked; Relentless schedule of Dota 2 competitive play ',
+    urlSafeTitle:
+      'no-rest-for-the-wicked-relentless-schedule-of-dota-2-competitive-play',
+    sectionIds: [9],
+    displaySectionId: 9,
+    displaySectionUrlSafeName: 'dota2',
+    publishedAt: 1731034560000,
+    headlineImagePath:
+      'https://static.staging.gosugamers.net/21/9b/3f/7995dfe830a0e6449e6292ceac8cecfeddad21481667abb7411dd8b5b1.webp',
+    headlineImageText: 'Dota 2 ',
+    squareHeadlineImageUrl: null,
+    teaser:
+      'In the high-stakes world of Dota 2, a player’s life is a relentless pursuit of excellence, but should it come at a mental health cost?',
+    url: '/dota2/features/69590-no-rest-for-the-wicked-relentless-schedule-of-dota-2-competitive-play',
+  },
+];
+
+export default articles;
