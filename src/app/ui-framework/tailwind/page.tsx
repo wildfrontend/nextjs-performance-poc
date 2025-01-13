5522d5404f32cb958527f258e9473d7d2df4b09c
Louis Luo <louis.luo@gosugamers.net>
2025-01-13 17:33:48 +0800

    Update tailwind css

--- src/app/ui-framework/tailwind/page.tsx
+++ src/app/ui-framework/tailwind/page.tsx
@@ -1,10 +1,11 @@
+import MainLayout from '@/components/ui-framework/tailwindcss/layouts/main';
 import '@/styles/globals.css';
 
 const Page: React.FC = () => {
   return (
-    <>
+    <MainLayout>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    </MainLayout>
   );
 };
 
