f20e976c4f31d44f71abb7668448711f09beebd4
Louis Luo <louis.luo@gosugamers.net>
2025-01-23 15:08:57 +0800

    Update ui

--- src/app/ui-framework/tailwind/page.tsx
+++ src/app/ui-framework/tailwind/page.tsx
@@ -6,11 +6,11 @@ const Page: React.FC = () => {
   return (
     <MainLayout>
       <div className="flex flex-col gap-8 w-full">
-        <h1>Tailwindcss Components</h1>
+        <h1 className='font-extrabold text-2xl sm:text-4xl'>Tailwindcss Components</h1>
         {componentSamples.map(({ id, title, component, docs }) => (
           <div className="flex flex-col gap-4" id={id} key={id}>
             <div className="flex flex-row items-center justify-between">
-              <h2>{title}</h2>
+              <h2 className='font-bold text-xl sm:text-2xl'>{title}</h2>
               <a
                 className="btn btn-secondary"
                 color="secondary"
