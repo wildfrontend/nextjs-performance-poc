e9379d44703736a4153706346937c446a5fa561b
Louis Luo <>
2025-02-22 23:26:24 +0800

    Fix query params suspense

--- src/app/antd/field-array/page.tsx
+++ src/app/antd/field-array/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import FieldArrayForm from '@/components/antd/field-array/form';
 
 const Page: React.FC = () => {
   return (
-    <div style={{ padding: '16px' }}>
-      <h1>Field Array </h1>
-      <FieldArrayForm />
-    </div>
+    <Suspense>
+      <div style={{ padding: '16px' }}>
+        <h1>Field Array </h1>
+        <FieldArrayForm />
+      </div>
+    </Suspense>
   );
 };
 
