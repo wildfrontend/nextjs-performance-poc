3e9c80cd5708c57191b48f9f12b92314e892cfaf
Louis Luo <>
2025-02-26 15:17:32 +0800

    Fix category type

--- src/components/react-query/products/navigation/category.tsx
+++ src/components/react-query/products/navigation/category.tsx
@@ -2,17 +2,22 @@ import Link from 'next/link';
 import { compile } from 'path-to-regexp';
 import React from 'react';
 
+type ProductCategory = {
+  slug: string;
+  name: string;
+  url: string;
+};
+
 const ProductCategory: React.FC = async () => {
   try {
-    const categories = await fetch(
+    const categories: ProductCategory[] = await fetch(
       'https://dummyjson.com/products/categories'
     ).then((res) => res.json());
     return (
       <ul className="menu menu-horizontal bg-base-200">
-        {/* @ts-ignore */}
         {categories.map((item) => {
           return (
-            <li key={item.id}>
+            <li key={item.slug}>
               <Link
                 href={compile('/react-query/products/categories/:category')({
                   category: item.slug,
