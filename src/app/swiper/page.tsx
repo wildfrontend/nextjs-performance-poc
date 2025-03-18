a98b681cc2828e7b7fb1294fd4a5018bb83fdb3e
Louis Luo <>
2025-03-18 13:05:12 +0800

    Update code

--- src/app/swiper/page.tsx
+++ src/app/swiper/page.tsx
@@ -2,9 +2,12 @@ import DefaultSlider from "@/components/swiper/default";
 
 const Page: React.FC = () => {
   return (
-    <>
+    <div>
       <DefaultSlider />
-    </>
+      <pre>
+        預設進入頁面會autoplay,LCP時間會很長
+      </pre>
+    </div>
   );
 };
 
--- src/app/swiper/performance/page.tsx
+++ src/app/swiper/performance/page.tsx
@@ -2,9 +2,12 @@ import PerformanceSlider from '@/components/swiper/performance';
 
 const Page: React.FC = () => {
   return (
-    <>
+    <div>
       <PerformanceSlider />
-    </>
+      <pre>
+        進頁面後透過setTimeout,用任務駐列 延遲autoplay開啟，可以先取得LCP 靜態資料 後面再啟動互動功能
+      </pre>
+    </div>
   );
 };
 
